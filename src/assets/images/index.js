22b2fb902cd68aae638ae466779ede6ae91cdfeb
Davronn <davronmahamadjonov8@gmai.com>
2024-02-28 17:59:00 +0300

    .

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,8 @@
-import headerIcon from './header-title.svg'
-import body1 from './body1.svg'
-import call from './call.svg'
+import headerIcon from "./header-title.svg";
+import body1 from "./body1.svg";
+import call from "./call.svg";
+import card1 from "./card1.svg";
+import card2 from "./card2.svg";
+import card3 from "./card3.svg";
 
-export {headerIcon,body1,call}
+export { headerIcon, body1, call, card1, card2, card3 };
--- src/components/body/About.jsx
+++ src/components/body/About.jsx
@@ -1,32 +1,73 @@
-import './Body.scss'
-import { body1, call } from "../../assets/images/index";
+import "./Body.scss";
+import { body1, call, card1 ,card2,card3} from "../../assets/images/index";
 
 function About() {
   return (
     <>
-      <div className="head">
-        <div className="container">
-          <div className="head_aside">
-            <h1>Quality cleaning for your home</h1>
-            <p className='hp'>
-              Condimentum mauris sit cursus amet id non neque pharetra nulla
-              ornare sed facilisis senectus dapibus nibh ultrices eget suscipit
-              aliquet et nulla magna lacus penatibus.
-            </p>
-            <div className="btns">
-              <button className="btn1">Get a free quote</button>
-              <div className="call">
-                <img src={call} alt="" />
-                <div>
-                  <p className='call-name'>CALL US NOW</p>
-                  <p className='call-num'>(414) 567 - 2109</p>
+      <div className="About">
+        <div className="head">
+          <div className="container">
+            <div className="head_aside">
+              <h1>Quality cleaning for your home</h1>
+              <p className="hp">
+                Condimentum mauris sit cursus amet id non neque pharetra nulla
+                ornare sed facilisis senectus dapibus nibh ultrices eget
+                suscipit aliquet et nulla magna lacus penatibus.
+              </p>
+              <div className="btns">
+                <button className="btn1">Get a free quote</button>
+                <div className="call">
+                  <img src={call} alt="" />
+                  <div>
+                    <p className="call-name">CALL US NOW</p>
+                    <p className="call-num">(414) 567 - 2109</p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+          <div className="bg-img">
+            <img src={body1} alt="" />
+          </div>
         </div>
-        <div className="bg-img">
-          <img src={body1} alt="" />
+        <div className="container">
+          <div className="aboutus">
+            <div className="aboutus_head">
+              <h2>About Us</h2>
+              <p className="aup">
+                Sagittis nibh scelerisque vitae eget vulputate sem elementum sed
+                neque nisi felis non ultrices massa id egestas quam velit
+                pretium nu.
+              </p>
+            </div>
+            <div className="aboutus_cards">
+              <div className="aboutus_cards-card">
+                <img src={card1} alt="" />
+                <p className="aucp">1. Schedule online</p>
+                <p className="aucpl">
+                  Sagittis nibh scelerisque vitae egetolment vulputate sem
+                  elementum sed n.
+                </p>
+              </div>
+              <div className="aboutus_cards-card">
+                <img src={card2} alt="" />
+                <p className="aucp">2. Pay online easliy</p>
+                <p className="aucpl">
+                  Vitae ut accumsan blandit ullamcorperolm suscipit dui gravida
+                  amet at nunc.
+                </p>
+              </div>
+              <div className="aboutus_cards-card">
+                <img src={card3} alt="" />
+                <p className="aucp">3. Get your house cleaned</p>
+                <p className="aucpl">
+                  Nunc maecenas sollicitudin metus tellus mattis sed porttitor
+                  cursus eleifend.
+                </p>
+              </div>
+            </div>
+            <div className="aboutus_btns"></div>
+          </div>
         </div>
       </div>
     </>
